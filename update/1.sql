b4dd7f980d92af19623b6fd6f20ee68a8c040e29
Hari R <52713882+hari1892@users.noreply.github.com>
2020-06-09 18:38:16 +0530

    Create 1.sql

---
+++ update/1.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `employees` (
+    emp_no      INT             NOT NULL,  -- UNSIGNED AUTO_INCREMENT??
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      ENUM ('M','F')  NOT NULL,  -- Enumeration of either 'M' or 'F'  
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)                   
+);
